e76ad3557279e99ce761ded76257bfb4874cf8fc
loonaticx <l.oony@aol.com>
2020-09-15 15:16:30 -0500

    Update convert script and add a cleanup script.

--- convert.py
+++ convert.py
@@ -1,13 +1,12 @@
 """
 	HyperModelConvert / HyperMC
+	convert.py
 	Tool to bulk convert Maya Binary and/or Panda3D egg/bam files.
 	Offers legacy support for bulk conversion of older bam file versions.
-	
+
 	Author: Loonatic
 	Date: 6/26/20
 """
-
-
 import subprocess, os, time, sys, re
 import argparse
 
@@ -22,34 +21,59 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--all-phases', '--all_phases', action='store_true', help='Convert all phase files folders. (3 to 14)')
 parser.add_argument('--selected_phases', '--phase', action='extend', nargs='+', type=str, metavar='3 3.5 4', help='List phase files folders to convert.')
+
+## Bam
 parser.add_argument('--bam2egg', '--to_egg', '--to-egg', action='store_true', help='Convert BAM file(s) into EGG file(s).')
 parser.add_argument('--egg2bam', '--to_bam', '--to-bam', action='store_true', help='Convert EGG file(s) into BAM file(s).')
+
+## Maya
 parser.add_argument('--egg2maya', action='store_true', help='Convert EGG file(s) into Maya Binary files.')
 parser.add_argument('--maya2egg', action='store_true', help='Convert Maya Binary files into EGG file(s).')
 parser.add_argument('--mayaver', '--mayaversion', '-mv', action='store', nargs='?', type=str, default='2016', metavar='MayaVersion', help='Use specific maya version. (Default is 2016)')
+
+## Obj
 parser.add_argument('--obj2egg', action='store_true', help='Convert OBJ files into EGG files.')
 parser.add_argument('--egg2obj', action='store_true', help='Convert EGG files into OBJ files.')
+
+## Fbx
+parser.add_argument('--fbx2egg', action='store_true', help='Convert FBX files into EGG files.')
+parser.add_argument('--egg2fbx', action='store_true', help='Convert EGG files into FBX files.')
+
+## Misc
 parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose output.')
+parser.add_argument('--overwrite', '-o', action='store_true', help='Overwrite preexisting files.')
 parser.add_argument('--recursive', '-r', action='store_true', help='Convert all folders in the directory, recursively. Typically used if there are models outside of "phase" folders.')
 parser.add_argument('--legacy', '--use-legacy', type=str, choices=['panda105', 'panda150', 'panda162', 'panda172'], action='store', help='Use Panda3D 1.0.5 or Panda3D 1.5.0 instead to convert LEGACY bams.')
-parser.add_argument('--panda_args', '--pargs', action='extend', nargs='+', type=str, help='Optional Panda3D args to pass. To get a full list, run `convert.py --pargs`.')
+#parser.add_argument('--panda_args', '--pargs', action='extend', nargs='+', type=str, help='Optional Panda3D args to pass. To get a full list, run `convert.py --pargs help`.')
+# Note: Removing pargs cause I dunno how to make this pretty at the moment
 
 args = parser.parse_args()
 
 # Config #
-#mayaArgs = "-a -m"
+#mayaArgs = "-a -m" ?
 allFiles = []
 if args.all_phases:
     args.selected_phases = ['3', '3.5', '4', '5', '5.5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
 
 # Tool settings
 settings = None # [ inputFile, outputFile, tool ]
+
+## Bam
 bam2egg = ['.bam', '.egg', 'bam2egg.exe']
 egg2bam = ['.egg', '.bam', 'egg2bam.exe']
+
+## Maya
 egg2maya = ['.egg', '.mb', 'egg2maya%s.exe' % args.mayaver]
 maya2egg = ['.mb', '.egg', 'maya2egg%s.exe' % args.mayaver]
+
+## Obj
 obj2egg = ['.obj', '.egg', 'obj2egg.exe']
 egg2obj = ['.egg', '.obj', 'egg2obj.exe']
+
+## Fbx
+fbx2egg = ['.fbx', '.egg', 'fbx2egg.exe']
+egg2fbx = ['.egg', '.fbx', 'egg2fbx.exe']
+
 # for now, egg is only accepted. we can allow for bams later.
 
 if args.bam2egg:
@@ -60,14 +84,25 @@ elif args.egg2maya:
 	settings = egg2maya
 elif args.maya2egg:
 	settings = maya2egg
-elif args.obj2egg:
+elif args.obj2egg: # obj->egg
 	settings = obj2egg
-elif args.egg2obj:
+elif args.egg2obj: # egg->obj
+	settings = egg2obj
+elif args.fbx2egg: # fbx->egg
+	settings = fbx2egg
+elif args.egg2fbx: # egg->fbx
 	settings = egg2obj
 
-optionalArgs = []
-if args.panda_args is not None:
-	optionalArgs = args.panda_args
+#optionalArgs = []
+overwriteArg = ''
+#if args.panda_args is not None:
+#	args.panda_args = [" -" + arg for arg in args.panda_args]
+	# Grr this is so hacky.
+#	optionalArgs = (args.panda_args)
+	
+if args.overwrite:
+	overwriteArg = '-o'
+	
 
 """
  # Panda3D Version Bam Support [Docme]
@@ -101,7 +136,7 @@ selectedPhases = args.selected_phases
 
 if (not recursive) and selectedPhases:
 	recursive = True # we're gonna do recursion on the phases anyway
-	
+
 if verbose and selectedPhases:
 	print(selectedPhases)
 
@@ -138,5 +173,7 @@ for file in allFiles:
 		continue
 	if verbose:
 		print("Converting %s..." % file)
-	subprocess.call(['%s/%s' % (defaultBin, tool), file] + optionalArgs + [newFile]) # 'bin/panda105/' / 'bam2egg[.exe]' optionalArgs file.bam file.egg
+	subprocess.call(['%s/%s' % (defaultBin, tool), file] + [overwriteArg] + [newFile]) # 'bin/panda105' / 'bam2egg[.exe]' optionalArgs file.bam overwriteArg newFile.egg
+	print(['%s/%s' % (defaultBin, tool), file] + [overwriteArg] + [newFile])
+	#print(['%s/%s' % (defaultBin, tool), ''.join(optionalArgs), file] + [overwriteArg] + [newFile])
 print("Conversion complete. Total time elapsed: %d ms" % (int(round(time.time() * 1000)) - start))
